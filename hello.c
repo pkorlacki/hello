47d819b82f19b56a8fc03f06988596a7c43f68ca
pkorlacki <pkorlacki>
2023-08-24 12:54:51 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Piotr Korlacki
+ * Date: 2023/08/24
+ *
+ * A simple program that prints my name
+ *
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Piotr Korlacki\n");
+
+    return 0;
+}
